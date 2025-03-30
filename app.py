9d1f1a1217cdcfe48f97de89f68ef0cbe805376f
Mikolaj <50639232+boras17@users.noreply.github.com>
2025-03-30 21:48:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,66 +3,76 @@ import pickle
 import pandas as pd
 from datetime import datetime
 startTime = datetime.now()
-# import znanych nam bibliotek
-
-import pathlib
-from pathlib import Path
-
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
-
-filename = "./model.sv"
+filename = "model2.sv"
 model = pickle.load(open(filename, 'rb'))
 # otwieramy wcześniej wytrenowany model
 
-pclass_d = {0:"Pierwsza",1:"Druga", 2:"Trzecia"}
-embarked_d = {0:"Cherbourg", 1:"Queenstown", 2:"Southampton"}
-sex_d = {0: 'Mężczyzna', 1: 'Kobieta'}
-class_d = {0: 'Klasa 1', 1: 'Klasa 2', 2: 'Klasa 3'}
+chestPainType = {0: 'Atypical Angina', 1: 'Non-Anginal Pain', 2: 'Asymptomatic', 3: 'Typical Angina'}
+sexD = {0: 'Male',1: 'Female'}
+restingECG = {0: 'Normal', 1: 'ST', 2: 'Left Ventricular Hypertrophy'}
+exerciseAngina = {0: 'Yes', 1: 'No'}
+stSlope = {0: 'Up',1: 'Flat',2: 'Down'}
+fastingBs = {0: 'Sugar level normal',1: 'Sugar level increased'}
+
+
 # o ile wcześniej kodowaliśmy nasze zmienne, to teraz wprowadzamy etykiety z ich nazewnictwem
 
-original_data_set = pd.read_csv('./DSP_1.csv', index_col=False)
+original_data_set = pd.read_csv('DSP_8.csv', index_col=False)
 
 age_min = int(original_data_set['Age'].min())
 age_max = int(original_data_set['Age'].max())
 
-sib_min = int(original_data_set['SibSp'].min())
-sib_max = int(original_data_set['SibSp'].max())
+restingbp_min = int(original_data_set['RestingBP'].min())
+restingbp_max = int(original_data_set['RestingBP'].max())
 
-par_min = int(original_data_set['Parch'].min())
-par_max = int(original_data_set['Parch'].max())
+cholesterol_min = int(original_data_set['Cholesterol'].min())
+cholesterol_max = int(original_data_set['Cholesterol'].max())
 
-fare_min = float(original_data_set['Fare'].min())
-fare_max = float(original_data_set['Fare'].max())
+maxHR_min = int(original_data_set['MaxHR'].min())
+maxHR_max = int(original_data_set['MaxHR'].max())
+
+min_oldpeak = float(0)
+max_oldpeak = float(original_data_set['Oldpeak'].max())
+
 
 def main():
 
-    st.set_page_config(page_title="Czy przeżyje?")
+    st.set_page_config(page_title="Will he get sick?")
     overview = st.container()
     left, right = st.columns(2)
     prediction = st.container()
 
+
+
     with left:
-        sex_radio = st.radio( "Płeć", list(sex_d.keys()), format_func=lambda x: sex_d[x])
-        embarked_radio = st.radio( "Port zaokrętowania", list(embarked_d.keys()), index=2, format_func=lambda x: embarked_d[x])
-        pclass_radio = st.radio("Klasa", list(class_d.keys()), format_func=lambda x: pclass_d[x])
-
+        sex_radio = st.radio("Sex", list(sexD.keys()), format_func=lambda x: sexD[x])
+        chestPainTypeRadio = st.radio("Chest pain type", list(chestPainType.keys()), index=2, format_func=lambda x: chestPainType[x])
+        restingECGRadio = st.radio("Resting ecg", list(restingECG.keys()), format_func=lambda x: restingECG[x])
+        exerciseAnginaRadio = st.radio("Exercise angina", list(exerciseAngina.keys()), format_func=lambda x: exerciseAngina[x])
+        stSlopeRadio = st.radio("Slope", list(stSlope.keys()), format_func=lambda x: stSlope[x])
+        fastingBsRadio = st.radio("FBS ", list(fastingBs.keys()), format_func=lambda x: fastingBs[x])
     with right:
-        age_slider = st.slider("Wiek", min_value=age_min, max_value=age_max, step=1)
-        sibsp_slider = st.slider("Liczba rodzeństwa i/lub partnera", min_value=sib_min, max_value=sib_max, step=1)
-        parch_slider = st.slider("Liczba rodziców i/lub dzieci", min_value=par_min, max_value=par_max, step=1)
-        fare_slider = st.slider("Cena biletu", min_value=fare_min, max_value=fare_max, step=0.1)
-    #data = [[pclass_radio, sex_radio,  age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]]
+        age_slider = st.slider("Age", min_value=age_min, max_value=age_max, step=1)
+        restingbp_slider = st.slider("Resting bp", min_value=restingbp_min, max_value=restingbp_max, step=1)
+        cholesterol_slider = st.slider("Choresterol", min_value=cholesterol_min, max_value=cholesterol_max, step=1)
+        maxHR_slider = st.slider("Max HR", min_value=maxHR_min, max_value=maxHR_max, step=1)
+        oldPeak_slider = st.slider("Old peak", min_value=min_oldpeak, max_value=max_oldpeak, step=0.1)
 
-    data = [[pclass_radio, sex_radio,  age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio]]
+    data = [[age_slider, sex_radio, chestPainTypeRadio, restingbp_slider,
+             cholesterol_slider, fastingBsRadio, restingECGRadio, maxHR_slider, exerciseAnginaRadio, oldPeak_slider, stSlopeRadio]]
+    print(data)
     survival = model.predict(data)
     s_confidence = model.predict_proba(data)
 
-    with prediction:
-        st.subheader("Czy taka osoba przeżyłaby katastrofę?")
-        st.subheader(("Tak" if survival[0] == 1 else "Nie"))
-        st.write("Pewność predykcji {0:.2f} %".format(s_confidence[0][survival][0] * 100))
 
+    with prediction:
+        st.subheader("Will heart disease occur?")
+        st.subheader(("Yes" if survival[0] == 1 else "No"))
+        st.write("Prediction certainty {0:.2f} %".format(s_confidence[0][survival][0] * 100))
+        if survival[0] == 1:
+            st.image("https://i.kym-cdn.com/entries/icons/mobile/000/026/489/crying.jpg")
+        else:
+            st.image("https://cdn.pixabay.com/photo/2022/11/26/22/35/cat-7618582_960_720.jpg")
 
 if __name__ == "__main__":
     main()
